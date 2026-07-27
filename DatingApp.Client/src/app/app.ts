2a7081cfaa764f3976b5f60222c78f028fb06d4f
Rose Beal <Rose.Beal@xpeditegoc.com>
2026-07-27 13:09:37 +0100

    section 3 angular skeleton

--- DatingApp.Client/src/app/app.ts
+++ DatingApp.Client/src/app/app.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,22 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 export class App implements OnInit{
   
   private http = inject(HttpClient);
-  protected readonly title = 'Dating App';
+  protected title = 'Dating App';
+  protected members = signal(<any>[]);
   
-  ngOnInit(): void {
-    this.http.get('http://localhost:5000/api/members').subscribe({
-      next: response => console.log(response),
-      error: error => console.log(error),
-      complete: () => console.log('Completed Request')
-    });
+  async ngOnInit() {
+    this.members.set(await this.getMembers())
+  }
+
+  async getMembers(){
+    try
+    {
+      return lastValueFrom(this.http.get('http://localhost:5000/api/members'));
+    }
+    catch(error)
+    {
+      console.log(error);
+      throw error;
+    }
   }
 }
