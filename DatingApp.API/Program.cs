3b8772d93220ec4ee837b673ab13a4e5547a66d7
Rose Beal <Rose.Beal@xpeditegoc.com>
2026-07-27 11:17:37 +0100

    add cors

--- DatingApp.API/Program.cs
+++ DatingApp.API/Program.cs
@@ -10,10 +10,14 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
